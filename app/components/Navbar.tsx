ff8c4685d4e5ee8a14738adbd2fbc8fba057a21c
MassiveMassimo <muhammad.hannan@ui.ac.id>
2023-10-05 16:48:45 +0700

    chore: fixed null handling

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,18 +5,18 @@ import { usePathname } from "next/navigation";
 import ThemeSelector from "./ThemeSelector";
 
 export default function Navbar() {
-  const pathname = usePathname()
-    .split("/")
-    .pop()
-    .split("-")
+  const pathname = usePathname();
+
+  const title = pathname
+    ?.split("/")
+    ?.pop()
+    ?.split("-")
     .map((word: string) => word.charAt(0).toUpperCase() + word.slice(1))
     .join(" ");
 
   return (
     <nav className="flex h-20 items-center justify-between px-10">
-      <h3 className="text-3xl font-semibold text-base-content-100">
-        {pathname}
-      </h3>
+      <h3 className="text-3xl font-semibold text-base-content-100">{title}</h3>
       <ThemeSelector />
     </nav>
   );
