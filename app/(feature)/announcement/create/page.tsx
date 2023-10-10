d3dec505d6749ad150a791c5a4049590ed242084
Bisma Khomeini <62429447+mbiskho@users.noreply.github.com>
2023-10-10 23:44:13 +0700

    [Feature] Announcement (#4)
    * feat:UI
    
    ui for announcement
    
    * fix iconify path
    
    remove /app/component
    
    * fix: color
    
    dan beberapa
    
    * try: post announcement
    
    otw
    
    * feat : create announcement
    
    * handle delete
    
    * feat : delete announcement
    
    * feat:announcement done
    
    done
    - create
    - delete
    - pin
    - detail
    - view all
    
    ---------
    
    Co-authored-by: adlanwalid <adlan.walid@ui.ac.id>

--- app/(feature)/announcement/create/page.tsx
+++ app/(feature)/announcement/create/page.tsx
@@ -1,14 +1,34 @@
-import { Metadata } from "next";
+// import { Metadata } from "next";
 
-export const metadata: Metadata = {
-  title: "Create Announcement | BNI Custody System",
-  description: "",
+// export const metadata: Metadata = {
+//   title: "Create Announcement | BNI Custody System",
+//   description: "",
+// };
+
+type CreateAnnouncementProps = {
+  isVisible: boolean;
+  onClose: () => void;
 };
+// const handleClose = (e: React.MouseEvent<HTMLDivElement>, onClose: CreateAnnouncementProps['onClose']) => {
+//   if ((e.target as HTMLElement).id === "wrapper") onClose();
+// }
+// const handleCloseX = (e) => {
+//   if(e.target.id == "wrapper") onClose()
+// }
 
-export default function CreateAnnouncement() {
+export default function CreateAnnouncement({ isVisible, onClose }: CreateAnnouncementProps) {
+  if (!isVisible) {
+    return null;
+  }
+
   return (
-    <main className="w-full grow rounded-tl-3xl bg-base-backdrop-200 p-10 shadow-2xl">
-      create announcement
-    </main>
+    <div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center" id="wrapper" onClick={() => onClose()}>
+      <div className="w-[600px] flex flex-col">
+        <button className="text-white text-xl place-self-end" onClick={() => onClose()}>
+          x
+        </button>
+        <div className="bg-white p-2 rounded">ini form anc</div>
+      </div>
+    </div>
   );
 }
