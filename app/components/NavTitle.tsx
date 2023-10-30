6f9335a93f221443ab709dcdf0146d1cc7e19832
Imo Madjid <mhmmadjid@gmail.com>
2023-10-30 11:54:50 +0700

    feat: added zustand, scrollbars, and utils

--- app/components/NavTitle.tsx
+++ app/components/NavTitle.tsx
@@ -7,9 +7,14 @@ import Iconify from "./Iconify";
 import { usePathname } from "next/navigation";
 import { Fragment } from "react";
 
+// stores
+import { useNasabahStore } from "@store/zustand";
+
 export default function NavTitle() {
   const pathname = usePathname();
   const segments = pathname.split("/").filter((segment) => segment !== "");
+
+  const nasabahName = useNasabahStore((state) => state.name);
 
   return (
     <>
@@ -28,7 +33,7 @@ export default function NavTitle() {
                 : "text-base-content-500"
             }`}
           >
-            {segment.replace(/-/g, " ")}
+            {segments[0] === "database" && index === 1 ? nasabahName : segment}
           </span>
         </Fragment>
       ))}
---
+++ app/store/zustand.ts
@@ -0,0 +1,24 @@
+import { create } from "zustand";
+
+import { Nasabah } from "@customTypes/types";
+
+type NasabahStore = Nasabah & {
+  setNasabah: (nasabah: Nasabah) => void;
+};
+
+export const useNasabahStore = create<NasabahStore>((set) => ({
+  id: "",
+  name: "",
+  address: "",
+  telephone: "",
+  expiry_date: "",
+  business_category: "",
+  service: "",
+  key_person_name: "",
+  key_person_dob: "",
+  key_person_hp: "",
+  created_at: "",
+  updated_at: null,
+
+  setNasabah: (nasabah) => set(nasabah),
+}));
